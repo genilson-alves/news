943e3dd1658ba720fc96a2127d012bd2eafb1a28
genilson-alves <genilson.alves9555@gmail.com>
2023-07-13 10:29:51 -0300

    Creating a news website!

---
+++ script.js
@@ -0,0 +1,8 @@
+// var url =
+//   "https://newsapi.org/v2/everything?" +
+//   "q=Brasil&" +
+//   "sortBy=popularity&" +
+//   "page=2&" +
+//   "apiKey=63a367487cc34c29955def57d90267d2";
+
+// fetch(url).then((res) => res.json().then((data) => console.log(data)));
