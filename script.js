ca465b6808ffec043c59f8b460604f089c21b17c
genilson-alves <genilson.alves9555@gmail.com>
2023-07-17 12:09:57 -0300

    Big progress today!

--- script.js
+++ script.js
@@ -1,65 +1,100 @@
 const weather_key = "e455787a22054869bc9135812231107";
 const news_key = "63a367487cc34c29955def57d90267d2";
+const locations = {
+  ip: "auto:ip",
+  sp: "sao+paulo",
+  rj: "rio+de+janeiro",
+  bra: "brasilia",
+};
 
-var news_url = `https://newsapi.org/v2/top-headlines?country=br&sortBy=popularity&apiKey=${news_key}`;
-var weather_url = `http://api.weatherapi.com/v1/current.json?key=${weather_key}&q=auto:ip&lang=pt`;
+var news_url = `https://newsapi.org/v2/top-headlines?apiKey=${news_key}&country=us&sortBy=popularity&pageSize=10`;
+
+Object.values(locations).forEach((location) => {
+  var weather_url = `https://api.weatherapi.com/v1/current.json?key=${weather_key}&q=${location}&lang=pt`;
+  fetch(weather_url).then((response) =>
+    response.json().then((response) => {
+      creatingWeather(
+        response.location.name,
+        response.current.temp_c,
+        response.current.condition.text,
+        response.current.feelslike_c,
+        response.current.condition.icon,
+        response.current.last_updated
+      );
+    })
+  );
+});
 
 fetch(news_url).then((response) =>
   response.json().then((response) => {
     response.articles.forEach((article) => {
-      creatingNews(article.author, article.title, article.urlToImage);
+      console.log(article);
+      creatingNews(
+        article.title,
+        article.url,
+        article.description,
+        article.author,
+        article.source.name,
+        article.publishedAt,
+        article.urlToImage
+      );
     });
   })
 );
 
-fetch(weather_url).then((response) =>
-  response.json().then((response) => {
-    creatingWeather(
-      response.location.name,
-      response.location.country,
-      response.current.temp_c,
-      response.current.condition.text,
-      response.current.feelslike_c,
-      response.current.condition.icon,
-      response.current.last_updated
-    );
-  })
-);
-
-const creatingNews = (author, title, image_url) => {
+const creatingNews = (title, link, detail, author, source, date, image_url) => {
   var news = document.createElement("div");
   var news_image = document.createElement("div");
   var news_img = document.createElement("img");
   var news_information = document.createElement("div");
-  var news_information_h3 = document.createElement("h3");
-  var news_information_p = document.createElement("p");
+  var news_information_div1 = document.createElement("div");
+  var news_information_div2 = document.createElement("div");
+  var news_info_h2 = document.createElement("h2");
+  var news_info_a = document.createElement("a");
+  var news_info_p = document.createElement("p");
+  var news_detail_span1 = document.createElement("span");
+  var news_detail_span2 = document.createElement("span");
 
   news.className = "news";
   news_image.className = "news-image";
   news_img.src = image_url
-    ? image_url
+    ? `${image_url}`
     : "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80";
   news_information.className = "news-information";
-  news_information_h3.textContent = title;
-  news_information_p.textContent = author;
+  news_information_div1.className = "news-info";
+  news_information_div2.className = "news-detail";
+
+  news_info_a.href = link;
+  news_info_a.textContent = title;
+  news_info_p.textContent = detail;
+  news_detail_span1.textContent = author
+    ? `${author},\n from ${source}.`
+    : `${source}`;
+  news_detail_span2.textContent = `Date: ${date}`;
 
   document.querySelector(".primary-content").appendChild(news);
   news.appendChild(news_image);
   news_image.appendChild(news_img);
   news.appendChild(news_information);
-  news_information.appendChild(news_information_h3);
-  news_information.appendChild(news_information_p);
+  news_info_h2.appendChild(news_info_a);
+  news_information_div1.appendChild(news_info_h2);
+  news_information_div1.appendChild(news_info_a);
+  news_information_div1.appendChild(news_info_p);
+  news_information_div2.appendChild(news_detail_span1);
+  news_information_div2.appendChild(news_detail_span2);
+  news_information.appendChild(news_information_div1);
+  news_information.appendChild(news_information_div2);
 };
 
 const creatingWeather = (
   city,
-  country,
   temperature,
   condition,
   feels_like,
   weather_image_url,
   last_updated
 ) => {
+  var weather_div = document.createElement("div");
   var weather_city = document.createElement("div");
   var weather_info = document.createElement("div");
   var weather_icon = document.createElement("div");
@@ -78,8 +113,8 @@ const creatingWeather = (
   weather_temp_span_feels.className = "feels-like";
   weather_last_updated.className = "last-updated";
 
-  weather_city.textContent = `${city} - ${country}`;
-  weather_icon_image.src = weather_image_url;
+  weather_city.textContent = `${city}`;
+  weather_icon_image.src = `https:${weather_image_url}`;
   weather_icon_span.textContent = condition;
   weather_temp_span_temp.textContent = `${Math.round(temperature)}°`;
   weather_temp_span_feels.textContent = `Feels Like: ${Math.round(
@@ -93,7 +128,8 @@ const creatingWeather = (
   weather_temp.appendChild(weather_temp_span_feels);
   weather_info.appendChild(weather_icon);
   weather_info.appendChild(weather_temp);
-  document.querySelector(".weather-1").appendChild(weather_city);
-  document.querySelector(".weather-1").appendChild(weather_info);
-  document.querySelector(".weather-1").appendChild(weather_last_updated);
+  weather_div.appendChild(weather_city);
+  weather_div.appendChild(weather_info);
+  weather_div.appendChild(weather_last_updated);
+  document.querySelector(".weather").appendChild(weather_div);
 };
