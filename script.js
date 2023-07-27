22a83df32de88efa8a8fbfbdac117c1a19574e55
genilson-alves <genilson.alves9555@gmail.com>
2023-07-27 15:10:21 -0300

    Optimizing some stuff!

--- script.js
+++ script.js
@@ -10,161 +10,122 @@ var side_news_url = `https://newsapi.org/v2/top-headlines?apiKey=${news_key}&cou
 var main_news_url = `https://newsapi.org/v2/top-headlines?apiKey=${news_key}&country=us&pageSize=14`;
 var moeda = `https://economia.awesomeapi.com.br/last/USD-BRL,CAD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,GBP-BRL,CNY-BRL`;
 
+const newsTemplate = document.querySelector("[data-news-template]");
+const newsContainer = document.querySelector("[data-news-container]");
+
+const weather_container = document.querySelector("[data-weather-container]");
+const weather_template = document.querySelector("[data-weather-template]");
+
 Object.values(locations).forEach((location) => {
   var weather_url = `https://api.weatherapi.com/v1/current.json?key=${weather_key}&q=${location}&lang=pt`;
   fetch(weather_url).then((response) =>
     response.json().then((data) => {
-      creatingWeather(
-        data.location.name,
-        data.current.temp_c,
-        data.current.condition.text,
-        data.current.feelslike_c,
-        data.current.condition.icon,
-        data.current.last_updated
+      const weather = weather_template.content.cloneNode(true).children[0];
+      const weather_city = weather.querySelector("[data-weather-city]");
+      const weather_image = weather.querySelector("[data-weather-city]");
+      const weather_current = weather.querySelector("[data-weather-current]");
+      const weather_temperature = weather.querySelector(
+        "[data-weather-temperature]"
       );
+      const weather_feels = weather.querySelector("[data-weather-feels]");
+      const weather_updated = weather.querySelector("[data-weather-updatd]");
     })
   );
 });
 
 fetch(main_news_url).then((response) =>
   response.json().then((data) => {
-    creatingMain(
-      data.articles[0],
-      data.articles[1],
-      data.articles[2],
-      data.articles[3]
-    );
-    data.articles.splice(0, 4);
     data.articles.forEach((article) => {
-      creatingNews(
-        article.title,
-        article.url,
-        article.description,
-        article.author,
-        article.source.name,
-        article.publishedAt,
-        article.urlToImage
-      );
+      const news = newsTemplate.content.cloneNode(true).children[0];
+      const news_image = news.querySelector("[data-news-image]");
+      const news_title = news.querySelector("[data-news-title]");
+      const news_description = news.querySelector("[data-news-description]");
+      const news_author = news.querySelector("[data-news-author]");
+      const news_source = news.querySelector("[data-news-source]");
+      const news_date = news.querySelector("[data-news-date]");
+
+      news_image.src = article.urlToImage;
+      news_title.href = article.url;
+      news_title.textContent = `${article.title.split(" - ")[0]}`;
+      news_description.textContent = article.description;
+      news_author.textContent = article.author
+        ? `${article.author.split(",", 2)}, `
+        : `${article.source.name}`;
+      news_source.textContent = article.author
+        ? `from ${article.source.name}`
+        : "";
+      news_date.textContent = `Date: ${article.publishedAt.slice(0, 10)}`;
+
+      newsContainer.append(news);
     });
   })
 );
 
-fetch(side_news_url).then((response) =>
-  response.json().then((data) => {
-    data.articles.forEach((article) => {
-      creatingSecondaryNews(article.title, article.url);
-    });
-  })
-);
+// fetch(side_news_url).then((response) =>
+//   response.json().then((data) => {
+//     data.articles.forEach((article) => {
+//       creatingSecondaryNews(article.title, article.url);
+//     });
+//   })
+// );
 
-fetch(moeda).then((response) =>
-  response.json().then((data) => {
-    Object.values(data).forEach((coin) => {
-      creatingMarket(coin.name, coin.ask, coin.create_date);
-    });
-  })
-);
+// fetch(moeda).then((response) =>
+//   response.json().then((data) => {
+//     Object.values(data).forEach((coin) => {
+//       creatingMarket(coin.name, coin.ask, coin.create_date);
+//     });
+//   })
+// );
 
-const creatingMarket = (code, ask, updated) => {
-  const tr = document.createElement("tr");
-  const td_1 = document.createElement("td");
-  const td_2 = document.createElement("td");
-  const td_3 = document.createElement("td");
+// const creatingMarket = (code, ask, updated) => {
+//   const tr = document.createElement("tr");
+//   const td_1 = document.createElement("td");
+//   const td_2 = document.createElement("td");
+//   const td_3 = document.createElement("td");
 
-  tr.appendChild(td_1);
-  tr.appendChild(td_2);
-  tr.appendChild(td_3);
+//   tr.appendChild(td_1);
+//   tr.appendChild(td_2);
+//   tr.appendChild(td_3);
 
-  td_1.textContent = `${code.split("/")[0]}`;
-  td_2.textContent = `R$${ask.slice(0, 4)}`;
-  td_3.textContent = updated.slice(10);
+//   td_1.textContent = `${code.split("/")[0]}`;
+//   td_2.textContent = `R$${ask.slice(0, 4)}`;
+//   td_3.textContent = updated.slice(10);
 
-  document.querySelector(".coin-table").appendChild(tr);
-};
+//   document.querySelector(".coin-table").appendChild(tr);
+// };
 
-const creatingMain = (col_1, col_2, col_3, col_4) => {
-  document.querySelector(".col-1 > img").src = col_1.urlToImage;
-  document.querySelector(".col-1 > a").textContent = `${
-    col_1.title.split(" - ")[0]
-  }`;
-  document.querySelector(".col-1 > a").href = col_1.url;
-  document.querySelector(".col-3 > img").src = col_2.urlToImage;
-  document.querySelector(".col-3 > a").textContent = `${
-    col_2.title.split(" - ")[0]
-  }`;
-  document.querySelector(".col-3 > a").href = col_2.url;
-  document.querySelector(".col-4 > img").src = col_3.urlToImage;
-  document.querySelector(".col-4 > a").textContent = `${
-    col_3.title.split(" - ")[0]
-  }`;
-  document.querySelector(".col-4 > a").href = col_3.url;
-  document.querySelector(".col-5 > img").src = col_4.urlToImage;
-  document.querySelector(".col-5 > a").textContent = `${
-    col_4.title.split(" - ")[0]
-  }`;
-  document.querySelector(".col-5 > a").href = col_4.url;
-};
+// const creatingMain = (col_1, col_2, col_3, col_4) => {
+//   document.querySelector(".col-1 > img").src = col_1.urlToImage;
+//   document.querySelector(".col-1 > a").textContent = `${
+//     col_1.title.split(" - ")[0]
+//   }`;
+//   document.querySelector(".col-1 > a").href = col_1.url;
+//   document.querySelector(".col-3 > img").src = col_2.urlToImage;
+//   document.querySelector(".col-3 > a").textContent = `${
+//     col_2.title.split(" - ")[0]
+//   }`;
+//   document.querySelector(".col-3 > a").href = col_2.url;
+//   document.querySelector(".col-4 > img").src = col_3.urlToImage;
+//   document.querySelector(".col-4 > a").textContent = `${
+//     col_3.title.split(" - ")[0]
+//   }`;
+//   document.querySelector(".col-4 > a").href = col_3.url;
+//   document.querySelector(".col-5 > img").src = col_4.urlToImage;
+//   document.querySelector(".col-5 > a").textContent = `${
+//     col_4.title.split(" - ")[0]
+//   }`;
+//   document.querySelector(".col-5 > a").href = col_4.url;
+// };
 
-const creatingNews = (title, link, detail, author, source, date, image_url) => {
-  var news = document.createElement("div");
-  var news_image = document.createElement("div");
-  var news_img = document.createElement("img");
-  var news_information = document.createElement("div");
-  var news_information_div1 = document.createElement("div");
-  var news_information_div2 = document.createElement("div");
-  var news_info_a = document.createElement("a");
-  var news_info_p = document.createElement("p");
-  var news_detail_span1 = document.createElement("span");
-  var news_detail_span2 = document.createElement("span");
-  var news_detail_span3 = document.createElement("span");
-  var news_detail_span4 = document.createElement("span");
+// const creatingSecondaryNews = (title, newsURL) => {
+//   secondaryNews = document.createElement("a");
 
-  news.className = "news";
-  news_image.className = "news-image";
-  news_img.src = image_url
-    ? `${image_url}`
-    : "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80";
-  news_information.className = "news-information";
-  news_information_div1.className = "news-info";
-  news_information_div2.className = "news-detail";
-  news_detail_span1.className = "news-detail-source";
+//   secondaryNews.textContent = `- ${title.split(" - ")[0]}`;
+//   secondaryNews.href = newsURL;
+//   secondaryNews.target = "_blank";
 
-  news_info_a.href = link;
-  news_info_a.target = "_blank";
-  news_info_a.textContent = `${title.split(" - ")[0]}`;
-  news_info_p.textContent = detail;
-  news_detail_span3.textContent = author
-    ? `${author.split(",", 2)}, `
-    : `${source}`;
-  news_detail_span4.textContent = author ? `from ${source}` : "";
-  news_detail_span2.textContent = `Date: ${date.slice(0, 10)}`;
-
-  document.querySelector(".primary-content").appendChild(news);
-  news.appendChild(news_image);
-  news_image.appendChild(news_img);
-  news.appendChild(news_information);
-  news_information_div1.appendChild(news_info_a);
-  news_information_div1.appendChild(news_info_p);
-  news_information_div2.appendChild(news_detail_span1);
-  news_detail_span1.appendChild(news_detail_span3);
-  news_detail_span1.appendChild(news_detail_span4);
-  news_information_div2.appendChild(news_detail_span2);
-  news_information.appendChild(news_information_div1);
-  news_information.appendChild(news_information_div2);
-
-  document.querySelector(".news-image").style.height =
-    document.querySelector(".news-information").offsetHeight;
-};
-
-const creatingSecondaryNews = (title, newsURL) => {
-  secondaryNews = document.createElement("a");
-
-  secondaryNews.textContent = `- ${title.split(" - ")[0]}`;
-  secondaryNews.href = newsURL;
-  secondaryNews.target = "_blank";
-
-  document.querySelector(".secondary-content-info").appendChild(secondaryNews);
-};
+//   document.querySelector(".secondary-content-info").appendChild(secondaryNews);
+// };
 
 const creatingWeather = (
   city,
